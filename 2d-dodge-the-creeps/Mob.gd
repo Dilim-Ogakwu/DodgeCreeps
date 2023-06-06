11204cdf3358d8f1b1180a4513098b33ddcbcaef
Dilim-Ogakwu <92801883+Dilim-Ogakwu@users.noreply.github.com>
2023-06-06 21:44:23 +0100

    Add files via upload

---
+++ 2d-dodge-the-creeps/Mob.gd
@@ -0,0 +1,16 @@
+extends RigidBody2D
+
+
+export var min_speed = 150
+export var max_speed = 250
+
+func _ready():
+	randomize()
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	pass # Replace with function body.
